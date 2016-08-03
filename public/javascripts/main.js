410e6929d54de5e4a4286b0cce72ea4fa7732ced
ShiYuanjun-Tim <syj_news@163.com>
2016-08-03 08:22:32 +0800

    add Offer

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,44 @@
-var React =require("react");
-var ReactDOM=require('react-dom');
-  
-  ReactDOM.render(
-  <h1>    ssww  </h1>,
-    document.getElementById('menu-toggle')
-  );
+ var ReactDOM = require('react-dom');
+
+ var React = require('react');
+
+ var OfferWorkArea = React.createClass({
+ 	render() {
+ 		return ( < div > OfferWorkArea < /div>);
+ 	}
+ });
+
+ var OfferEditor = React.createClass({
+ 	render() {
+ 		return ( < div > OfferEditor < /div>);
+ 	}
+ });
+
+ var OfferHome = React.createClass({
+ 	render() {
+ 		return ( < div > OfferHome < /div>);
+ 	}
+ });
+
+ var Offer = React.createClass({
+ 	render() {
+ 		var  attr ={textOverflow: "ellipsis", whiteSpace: "pre-wrap", overflow: "hidden"}; 
+ 		var enrollBy="fsdfsfs";
+ 		var rank=1;
+ 		return ( 
+ 		<a href="#" className="btn btn-default truncate" className="thumbnail">
+			<img src="/static/images/schools/andalue.jpg" alt=""/>
+			<strong className="text-left" style={attr}>title</strong><br/>
+			<small className="text-left">录取院校：{enrollBy}</small><br/>
+			<small className="text-left">综合排名第{rank}位</small>
+		</a>);
+ 	}
+ });
+
+
  
-// document.write("sww")
+ ReactDOM.render(  <Offer/>,
+ 	document.getElementById("reactContainer")
+ );
+
+ // document.write("sww")
