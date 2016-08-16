7c735a2035f026981f676a6cca7197c7e5f08363
ShiYuanjun-Tim <syj_news@163.com>
2016-08-16 21:57:15 +0800

    rename www to server.js and put to root folder

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '越扬教育' });
+  res.render('index', { title: '领悠' });
 });
 
  
